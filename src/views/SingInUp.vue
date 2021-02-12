da119bf60a855510911ccd5a710e218f1d255bb7
LuiDev2001 <luisqrja@gmail.com>
2021-02-11 22:01:38 -0500

    Create footer structure and styles for page Login & Register

--- src/views/SingInUp.vue
+++ src/views/SingInUp.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home">
     <PxLogin />
+    <PxFooter />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import PxLogin from "@/components/PxLogin";
+import PxFooter from "@/components/PxFooter";
+
 export default {
   name: "Home",
   components: {
     PxLogin,
+    PxFooter,
   },
 };
 </script>
