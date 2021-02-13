46f2044dfc9f9d7a235b42746c2bcc0965fff4a3
Producciones AG <desarrolloweb@agvideo.tv>
2021-02-12 20:34:18 -0500

    se agrega formulario de registro, solo el html

--- src/components/PxLogin.vue
+++ src/components/PxLogin.vue
@@ -1,12 +1,63 @@
 <template>
-  <div class="hello">
-    <h2>Formulario de Login</h2>
-  </div>
+    <div class="login">
+        <form action method="post" autocomplete="on" @submit.prevent="login">        
+            <div class="input-group">
+                <label id="email-lbl" for="email" class="input-group__label" >
+                    Correo electr&oacute;nico</label>
+                <input type="email" name="email" id="email" v-model="form.email" class="input-group__input" placeholder="Correo electr&oacute;nico" />
+            </div>
+            <div class="input-group">
+                <label id="password-lbl" for="password" class="input-group__label" >
+                    Contraseña</label>
+                <input type="password" name="password" id="password" v-model="form.password" class="input-group__input" placeholder="Contraseña" />
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="button" class="link">
+                        ¿Olvidaste tu contraseña?
+                    </button>
+                </div>
+                <div class="col">
+                    <input type="checkbox" class="input-group__check" 
+                        id="remember" v-model="form.remember" />
+                    <label class="input-group__check-label" for="remember">
+                        Mantenerme conectado
+                    </label>                
+                </div>
+            </div>        
+            <button type="submit" class="button">
+                Ingresar
+            </button>
+        </form>
+        <p class="text-center">Tambien puedes iniciar con tus redes</p>
+        <button type="submit" class="button" @click="logInWithFacebook">
+            <i class="fa fa-facebook"></i>        
+            Ingresar con Facebook
+        </button>
+        <button type="submit" class="button">
+            <i class="fa fa-google"></i>
+            Ingresar con Google
+        </button>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "PxLogin",
+    name: "PxLogin",
+    data(){
+        return{
+            form:{
+                email:'',
+                password:'',
+                remember:false
+            }           
+        }
+    },
+    methods: {
+        login(){
+            console.log(this.form);
+        }
+    }
 };
 </script>
 
