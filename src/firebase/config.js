c73a92a6238e38445d0f3536ce2626279003cc58
LuiDev2001 <luisqrja@gmail.com>
2021-02-12 20:28:58 -0500

    Add package firebase, vuelidate and add and config credentials for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBskyfLayRHR-4pt0iJs95J3jFgXOymi_g",
+  authDomain: "comfeco-backend.firebaseapp.com",
+  projectId: "comfeco-backend",
+  storageBucket: "comfeco-backend.appspot.com",
+  messagingSenderId: "32525832017",
+  appId: "1:32525832017:web:03aa14503fabcb31230d70",
+};
+
+export default { firebaseConfig };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import firebase from "firebase";
+import firebaseConfig from "./firebase/config.js";
+
+//Inicializando nuestro app con firebase
+firebase.initializeApp(firebaseConfig);
 
 createApp(App)
   .use(router)
