f3e5055c3ddac528705b8b7230ac656aeed4ea94
jeffamed <jeffamed@gmail.com>
2021-02-19 11:12:03 -0600

    se agrego el componente de VComunidades para mostrar a las comunidades y se agrego unos estilos para mostrar el componente en un aside

---
+++ src/components/PxComunidades.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="flex comunidad">
+        <div class="comunidad__img">
+            <img :src="imagen" alt="imagen de la comunidad">
+        </div>
+        <div class="comunidad__name">
+            <h4 v-text="titulo"></h4>
+        </div>
+        <div>
+            <a class="comunidad__btn" :href="url">Unirme</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "VComunidad",
+    props:{
+        imagen: String,
+        titulo: String,
+        url: String
+    }
+}
+</script>
+<style lang="scss">
+    .flex {
+        display: flex;
+
+    }
+    .comunidad{
+        border-bottom: 1px solid hsl(280, 5, 30);
+        margin: auto;
+        padding: 25px;
+        &__img{
+            border-radius: 50%;
+            margin-right: 2rem;
+        }
+        &__name{
+            font-weight: 800;
+            margin-right: 2rem;
+        }
+        &__btn{
+            color: hsl(280, 50, 50);
+            text-decoration: none;
+            
+            &:hover{
+                color: hsl(280, 70, 50);
+                cursor: pointer;
+            }
+        }
+    }
+</style>
--- src/components/PxDashboard.vue
+++ src/components/PxDashboard.vue
@@ -3,13 +3,30 @@
     <h1>This is a private page</h1>
     <button @click="logOut">LogOut</button>
   </div>
+  <div class="aside">
+    <div class="aside__header">
+        <h5>Comunidades</h5>
+        <a href="#">Ver más</a>
+    </div>
+    <VComunidad :imagen="logo" :titulo="titulo" :url="url"></VComunidad>
+  </div>
 </template>
 
 <script>
 import Autenticacion from "@/firebase/auth/autentication.js";
-
+import VComunidad from "@/components/PxComunidades";
 export default {
   name: "private",
+  data(){
+    return{
+      logo:'https://picsum.photos/50/50',
+      titulo: 'Comunidad 1',
+      url: 'https:www.google.com'
+    }
+  },
+  components:{
+    VComunidad
+  },
   methods: {
     logOut() {
       this.authClass.singOutOfAccount().then(() => {
@@ -25,3 +42,20 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+  .aside{
+    width: 20%;
+    height: 40vh;
+    margin: 1%;
+    padding: 1%;
+    background-color: hsl(0, 1, 70);
+    color: white;
+    overflow: auto;
+    &__header{
+      display: flex;
+      width: 100%;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+  }
+</style>
