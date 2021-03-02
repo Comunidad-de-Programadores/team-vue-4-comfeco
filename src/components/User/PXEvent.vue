cd183d2c711bca050e506c56e4f5483873a290e8
Yefry Moraga <jeffamed@gmail.com>
2021-03-01 21:56:25 -0600

    se agrego el componente Evento, falta crear la vista Mas Evento y alinear a la izq

---
+++ src/components/User/PXEvent.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="evento">
+        <div class="evento__header">
+            <h5 class="evento__title">Evento de tu Interes</h5>
+            <a href="#" class="evento-link">Ver más</a>
+        </div>
+        <div class="evento__body">
+            <img class="evento__body__img" src="@/assets/images/Logo-comfeco-opción-1.png" alt="Logo del Evento">
+            <h5 class="evento__title evento__title-ml1">Comunidad Fest and Code</h5>
+            <div class="center-btn">
+                <button class="evento__body__btn">Mas Información</button>
+            </div>
+        </div>
+        <div class="evento__body">
+            <img class="evento__body__img" src="@/assets/images/Logo-comfeco-opción-1.png" alt="Logo del Evento">
+            <h5 class="evento__title evento__title-ml1">Comunidad Fest and Code</h5>
+            <div class="center-btn">
+                <button class="evento__body__btn">Mas Información</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "PxEvent",
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .evento{
+        width: 25%;
+        height: 60vh;
+        padding: 1rem;
+        background: hsl(0, 1, 80);
+        overflow: auto;
+        &__title{
+            color: #ffffff;
+            &-ml1{
+                margin-left: 0.5rem;
+            }
+        }
+        &__header{
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 3rem;
+        }
+        &__body{
+            width: 100%;
+            height: 60%;
+            margin-bottom: 2rem;
+            background: linear-gradient(to bottom right, hsl(290, 50, 50), hsl(285,50,50), hsl(280,50,50));
+            &__img{
+                width: 100%;
+                margin-bottom: 2rem;
+            }
+            &__btn{
+                padding: 1em;
+                border-radius: 0.5em;
+                font-weight: 700;
+                background: hsl(0, 1, 65);
+                color: #ffffff;
+            }
+            .center-btn{
+                margin-top: 1rem;
+                text-align: center;
+            }
+        }
+    }
+</style>
--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -3,18 +3,21 @@
     <PxInsignia />
 
     <PxEditInfoUser />
+    <PxEvent/>
   </section>
 </template>
 
 <script>
 import PxInsignia from "@/components/User/PxInsignia";
 import PxEditInfoUser from "@/components/User/PxEditInfoUser";
+import PxEvent from "@/components/User/PXEvent";
 
 export default {
   name: "MyAccount",
   components: {
     PxInsignia,
     PxEditInfoUser,
+    PxEvent,
   },
 };
 </script>
